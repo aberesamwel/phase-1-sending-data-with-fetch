a2ffaa18ce2986ca58977eb521fe75f183c99528
Samwel Maina <abere.maina@gmail.com>
2025-06-27 20:28:09 +0300

    second commit

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email
+    })
+  })
+  .then(response => response.json())
+  .then(data => {
+    // append new id to the DOM
+    const body = document.querySelector('body');
+    body.innerHTML += `<p>${data.id}</p>`;
+  })
+  .catch(error => {
+    // append error message to the DOM
+    const body = document.querySelector('body');
+    body.innerHTML += `<p>${error.message}</p>`;
+  });
+}
+
